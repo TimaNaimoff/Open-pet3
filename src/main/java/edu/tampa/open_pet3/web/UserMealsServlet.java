fd8abafbaf2b0f1f5a447d326427cab20de49afa
Temur Vegas <timanaimoff@gmail.com>
2023-02-09 20:06:29 +0500

    SLF4j and Log4j in praktik

--- src/main/java/edu/tampa/open_pet3/web/UserMealsServlet.java
+++ src/main/java/edu/tampa/open_pet3/web/UserMealsServlet.java
@@ -1,5 +1,6 @@
 package edu.tampa.open_pet3.web;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -9,13 +10,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
-public class UserServlet extends HttpServlet {
-    private static final Logger LOG= getLogger(UserServlet.class);
+public class UserMealsServlet extends HttpServlet {
+    private static final Logger LOG= LoggerFactory.getLogger(UserMealsServlet.class);
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        LOG.debug("Redirect to userList");
-        req.getRequestDispatcher("/User.jsp").forward(req,resp);
+        LOG.info("redirect started!");
+        req.getRequestDispatcher("/UserMeal.jsp").forward(req,resp);
     }
 }
